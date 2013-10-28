871785f2b1dcff500a6547f040814d6b00f05c89
hacker1999 <tz46.78@gmail.com>
2013-10-28 11:59:39 -0700

    Сэкономил 1 байт

--- css_grid_generator.py
+++ css_grid_generator.py
@@ -12,7 +12,7 @@ try:
     os.makedirs(directory + '/css')
 except OSError:
     pass
-css = '.container{width:%spx;margin:auto}.column{float:left;margin: 0 %spx 0 %spx}'
+css = '.container{width:%spx;margin:auto}.column{float:left;margin:0 %spx 0 %spx}'
 css = css % (width, margin_left, margin_right)
 i = 1
 col_width = width / cols
